0582b71075cf0c0c4d4f5b23071239f985fbedf7
thanos <thanosv@gmail.com>
2026-06-12 11:01:19 -0400

    bumped elixir versions

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule ExAzure.MixProject do
     [
       app: :ex_azure,
       version: @version,
-      elixir: "~> 1.16",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       name: "ExAzure",
